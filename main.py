da7cc0d2a130a8ea8b77dfa0ac3b35f5a182c438
benny_neiman <bn161299@gmail.com>
2025-10-29 11:20:49 +0200

    menu

--- main.py
+++ main.py
@@ -14,8 +14,28 @@ def showing_list(todo_list):
     for i in todo_list:
         index+=1
         print (index,i)
-    
 
+
+def menu():
+    flag=True
+    while flag:
+        user_choice=int(input("""Hey what would you like to do? 
+        1.Add task
+        2.View list
+        3.Exit
+        :"""))
+        match user_choice:
+            case 1: 
+                adding_items(todo_list)
+            case 2:
+                showing_list(todo_list)
+            case 3:
+                flag=False
+                print("bye bye")
+                
+            
+        
+        
 
 
 
